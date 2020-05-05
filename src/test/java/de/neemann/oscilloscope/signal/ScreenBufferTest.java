222eb2e8a71d3caf9d5dc0b3da664c8da0e89c7a
hneemann <helmut.neemann@arcor.de>
2020-05-05 20:16:14 +0200

    fixes a broken test case

--- src/test/java/de/neemann/oscilloscope/signal/ScreenBufferTest.java
+++ src/test/java/de/neemann/oscilloscope/signal/ScreenBufferTest.java
@@ -5,10 +5,12 @@ import junit.framework.TestCase;
 
 import java.awt.*;
 
+import static de.neemann.oscilloscope.signal.ModelTimeCalc.MIN_TRACE_BRIGHT;
+
 public class ScreenBufferTest extends TestCase {
 
     public void testColorTable() {
-        assertEquals(Style.SCREEN.getColor(), ScreenBuffer.SPEEDCOLOR[ScreenBuffer.SPEEDCOLOR.length - 1]);
+        assertEquals(Style.SCREEN.getColor().getGreen() + MIN_TRACE_BRIGHT, ScreenBuffer.SPEEDCOLOR[ScreenBuffer.SPEEDCOLOR.length - 1].getGreen());
         assertEquals(Color.GREEN, ScreenBuffer.SPEEDCOLOR[0]);
     }
 
